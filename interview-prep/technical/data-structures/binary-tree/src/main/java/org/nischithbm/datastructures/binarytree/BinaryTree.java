4e94dc230137923aeba44e24b0f9fdf44f659d61
Nischith B.M <nischithbm@gmail.com>
2014-06-08 13:32:25 +0530

    Adding BinaryTree project structure

---
+++ interview-prep/technical/data-structures/binary-tree/src/main/java/org/nischithbm/datastructures/binarytree/BinaryTree.java
@@ -0,0 +1,13 @@
+package org.nischithbm.datastructures.binarytree;
+
+/**
+ * Hello world!
+ *
+ */
+public class BinaryTree 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ interview-prep/technical/data-structures/binary-tree/src/test/java/org/nischithbm/datastructures/binarytree/BinaryTreeTest.java
@@ -0,0 +1,38 @@
+package org.nischithbm.datastructures.binarytree;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BinaryTreeTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BinaryTreeTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BinaryTreeTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
